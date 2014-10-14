a2a6676fe54f06185428891878f44b7ef604481c
michail <michailgames@yahoo.pl>
2014-10-15 01:16:38 +0200

    [#4] created Article class

---
+++ article.rb
@@ -0,0 +1,51 @@
+class Article
+  attr_reader :title
+  attr_reader :content
+  attr_reader :author
+  attr_reader :created_at
+  
+  def initialize(title, content, author="")
+    @title = title
+    @content = content
+    @author = author
+    @likes = 0
+    @dislikes = 0
+    @created_at = Time.now
+  end
+  
+  def like!
+    @likes += 1
+  end
+  
+  def dislike!
+    @dislikes += 1
+  end
+  
+  def positive_votes
+    @likes - @dislikes
+  end
+  
+  def votes
+    @likes + @dislikes
+  end
+  
+  def shortened_to(limit)
+    if @content.size <= limit
+      @content
+    else 
+      @content[0, (limit >= 3 ? limit - 3 : 0)] + "..."
+    end
+  end
+  
+  def include?(text)
+    @content.include?(text)
+  end
+  
+  def words
+    @content.split(/\W+/)
+  end
+  
+  def distinct_words
+    (words.map { |w| w.downcase }).uniq
+  end
+end
