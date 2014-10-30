ff5e823b8c731268292684a0b0c9b0ea2f67959f
michail <michailgames@yahoo.pl>
2014-10-31 00:36:48 +0100

    [#12] created ArticleFilesystem class and write_to_file method

---
+++ article_filesystem.rb
@@ -0,0 +1,12 @@
+class ArticleFilesystem
+
+  ARTICLES_FOLDER = './articles/'
+  
+  def write_to_file(article)
+    filename = article.title.downcase.split.join("_")
+    File.open(ARTICLES_FOLDER + filename, 'w') do |file|
+      file.write("#{article.author}|#{article.title}|#{article.content}|" + \
+                "#{article.likes}|#{article.dislikes}")
+    end
+  end
+end
