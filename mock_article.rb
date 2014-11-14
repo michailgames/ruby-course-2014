62ded0c2aaf140bb09a2c2d4b2783b6719e4c25f
michail <michailgames@yahoo.pl>
2014-11-14 19:43:24 +0100

    [#17] moved string generating part of MockArticle to 'bla-bla' gem

--- mock_article.rb
+++ mock_article.rb
@@ -1,51 +1,14 @@
 require_relative 'article.rb'
+require 'bla-bla'
 
 class MockArticle < Article
-  WORD_LIST = ["legendary", "awesome", "wait", "for", "it", "brother",
-               "story", "cool", "fox", "great", "white", "polar", "bear",
-               "university", "such", "educating"]
-
   def initialize
-    @author = generate_author
-    @title = generate_title
-    @content = generate_content
+    super(BlaBla.title, BlaBla.content, BlaBla.author)
     @likes = random_number
     @dislikes = random_number
-    @created_at = Time.now
   end
                
   def random_number
     rand(101)
-  end
-  
-  def generate_author
-    WORD_LIST.sample.capitalize + " " + WORD_LIST.sample.capitalize
-  end
-  
-  def generate_title
-    n = rand(4)
-    s = WORD_LIST.sample.capitalize 
-    n.times do 
-      s += " " + WORD_LIST.sample
-    end
-    s
-  end
-  
-  def generate_sentence
-    n = 4 + rand(5)
-    s = WORD_LIST.sample.capitalize 
-    n.times do 
-      s += " " + WORD_LIST.sample
-    end
-    s + "."
-  end
-  
-  def generate_content
-    n = rand(4)
-    s = generate_sentence 
-    n.times do 
-      s += " " + generate_sentence
-    end
-    s
   end
 end
--- spec/unit/mock_article_spec.rb
+++ spec/unit/mock_article_spec.rb
@@ -7,64 +7,7 @@ class MockArticleSpec < MiniTest::Test
     @article = MockArticle.new
   end
   
-  def test_initialize
-    _test_title(@article.title)
-    _test_author(@article.author)
-    _test_content(@article.content)
-    _test_random_number(@article.likes)
-    _test_random_number(@article.dislikes)
-  end
-  
-  def _test_random_number(n)
-   assert(n.between?(0, 100))
-  end
-  
   def test_random_number
-    _test_random_number(@article.random_number)
+    assert(@article.random_number.between?(0,100))
   end
-  
-  def _test_author(author)
-    name_parts = author.split
-    assert_equal(2, name_parts.size)
-    name_parts.each do |name_part|
-      assert_equal(name_part, name_part.capitalize)
-    end
-  end
-  
-  def test_generate_author
-    _test_author(@article.generate_author)
-  end
-  
-  def _test_title(title)
-    title_parts = title.split
-    assert(title_parts.size.between?(1, 5)) 
-    assert_equal(title_parts.first, title_parts.first.capitalize)
-  end
-  
-  def test_generate_title
-    _test_title(@article.generate_title)
-  end
-  
-  def _test_sentence(sentence)
-    words = sentence.split
-    assert(words.size.between?(4, 9))
-    assert_equal(words.first, words.first.capitalize)
-    assert_equal(sentence.chars.last, ".")
-  end
-  
-  def test_generate_sentence
-    _test_sentence(@article.generate_sentence)
-  end
-  
-  def _test_content(content)
-    sentences = content.split(/(?<=\. )/)
-    assert(sentences.size.between?(1, 5)) 
-    sentences.each do |sentence|
-      _test_sentence(sentence.strip)
-    end
-  end
-  
-  def test_generate_content
-    _test_content(@article.generate_content)
-  end 
 end
